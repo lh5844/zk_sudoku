4cbb5bd2072a21746166975809b1fff344a553c3
Lucy <lh5844rit@gmail.com>
2025-12-03 16:11:24 -0500

    added sha 256 hash and random salt to commit the permutated solution board

--- prover_commit.js
+++ prover_commit.js
@@ -1,5 +1,8 @@
 import { solutionBoard, prefilledCells, displayBoard } from "./sudoku_display.js";
 
+let permutatedSolution = null;
+let committedCells = [];
+
 // Fisher-Yates array shuffling algorithm for random permutation
 function shuffleArray(array){
     for (let i = array.length -1; i > 0; i--){
@@ -23,10 +26,59 @@ const permuteButton = document.getElementById("permuteButton");
 permuteButton.addEventListener("click", ()=>{
     const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const permutation = shuffleArray([...digits]);
-    const permutatedSolution = permutateBoard(solutionBoard, permutation);
+    permutatedSolution = permutateBoard(solutionBoard, permutation);
     
     displayBoard(permutatedSolution, "solutionBoardDiv", prefilledCells);
     document.getElementById("proverBoardTitle").textContent = "Permutated Board";
 
     permuteButton.disabled = true;
+    commitButton.disabled = false; // can't commit until board is permutated
 });
+
+// commitment helpers 
+function generateRandomSalt(len=16){
+    const bytes = crypto.getRandomValues(new Uint8Array(len));
+    // format into hex 
+    return [...bytes].map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
+async function sha256Hash(message){
+    const buffer = new TextEncoder().encode(message);
+    const hash = await crypto.subtle.digest("SHA-256", buffer);
+    return [...new Uint8Array(hash)].map(x => x.toString(16).padStart(2,"0")).join("");
+}
+
+// commitment every cell of permutated solution board
+async function commitBoard(board){
+    committedCells = [];
+    for (let row = 0; row < 9; row++){
+        for (let col = 0; col < 9; col++){
+            const cell = board[row][col];
+            const salt = generateRandomSalt();
+            const msg = cell + "|" + salt;
+            const hash = await sha256Hash(msg);
+
+            committedCells.push({
+                row: row,
+                col: col, 
+                hash, 
+                salt,
+                cell: cell
+            });
+        }
+    }
+    displayCommitments();
+}
+
+function displayCommitments(){
+    const box = document.getElementById("commitmentBox");
+    box.innerHTML = committedCells.map(c =>
+        `(${c.row},${c.col}) → ${c.hash}`
+    ).join("<br>");
+}
+
+const commitButton = document.getElementById("commitButton");
+commitButton.onclick = async () => {
+  await commitBoard(permutatedSolution);
+  commitButton.disabled = true;
+};
